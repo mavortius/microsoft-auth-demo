8f1d24e3fd8acfb8ecc54be7252897b8db5e3983
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2018-11-28 14:19:21 -0200

    Added fake role 'USER' for testing

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
 import java.security.Principal;
+import java.util.Base64;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,4 +20,12 @@ public class HomeController {
   public ResponseEntity<Principal> home(@AuthenticationPrincipal Principal principal) {
     return ResponseEntity.ok(principal);
   }
+
+  @GetMapping("/username")
+  public String name(@AuthenticationPrincipal Jwt jwt) {
+    return new String(Base64Utils.decodeFromString(jwt.getSubject()));
+  }
+
+
+
 }
--- src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -1,9 +1,18 @@
 package com.example.demo.security;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,11 +22,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     http
         .authorizeRequests().anyRequest().authenticated()
         .and()
-        .oauth2ResourceServer().jwt();
+        .oauth2ResourceServer().jwt().jwtAuthenticationConverter(grantedAuthoritiesExtractor());
   }
 
   @Override
-  public void configure(WebSecurity web) throws Exception {
+  public void configure(WebSecurity web) {
     web.ignoring().antMatchers("/resources/**");
+  }
+
+  Converter<Jwt, AbstractAuthenticationToken> grantedAuthoritiesExtractor() {
+    return new GrantedAuthoritiesExtractor();
+  }
+
+  static class GrantedAuthoritiesExtractor extends JwtAuthenticationConverter {
+
+    @Override
+    protected Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
+      Collection<String> authorities = Collections.singleton("ROLE_USER");
+
+      return authorities.stream()
+          .map(SimpleGrantedAuthority::new)
+          .collect(Collectors.toList());
+    }
   }
 }
