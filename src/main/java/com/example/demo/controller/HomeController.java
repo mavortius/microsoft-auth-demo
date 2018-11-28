01dc2df010d4859e9d1183c760e15654e28c5f78
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2018-11-28 18:20:09 -0200

    Added 'username' endpoint

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,9 @@
 package com.example.demo.controller;
 
-import java.io.IOException;
 import java.security.Principal;
-import java.util.Base64;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +19,7 @@ public class HomeController {
 
   @GetMapping("/username")
   public String name(@AuthenticationPrincipal Jwt jwt) {
-    return new String(Base64Utils.decodeFromString(jwt.getSubject()));
+    return jwt.getClaimAsString("preferred_username");
   }
 
 
