efb2f3a4613d8ffbe853165cc237e19ea8cc14b6
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2018-11-28 11:31:37 -0200

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import java.security.Principal;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+  @GetMapping
+  public ResponseEntity<Principal> home(@AuthenticationPrincipal Principal principal) {
+    return ResponseEntity.ok(principal);
+  }
+}
